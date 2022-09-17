870e7476a99dacb2153a5ffc2f1580db4166a45a
Elijah Cobb <elijah.cobb@vercel.com>
2022-09-17 15:05:07 -0400

    fixed referrer

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -4,6 +4,8 @@ import { FaInfoCircle } from "react-icons/fa";
 export function Nav() {
 	return <nav className={styles.container}>
 		<h1>Wordle</h1>
-		<FaInfoCircle />
+		<a href="https://elijahcobb.dev" target={'_blank'} rel="noreferrer">
+			<FaInfoCircle />
+		</a>
 	</nav>
 }
