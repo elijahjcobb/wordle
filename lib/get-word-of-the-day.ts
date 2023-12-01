9e1e549424ebe6c21f6171a93dcf5967564e3cfc
Elijah Cobb <elijahjcobb@gmail.com>
2023-11-30 17:11:50 -0800

    add log

--- lib/get-word-of-the-day.ts
+++ lib/get-word-of-the-day.ts
@@ -7,25 +7,30 @@ export interface WordOfTheDay {
 }
 
 export async function getWordOfTheDay(): Promise<WordOfTheDay> {
-  const today = new Date();
-  const url = `https://www.nytimes.com/svc/wordle/v2/${today.getFullYear()}-${
-    today.getMonth() + 1
-  }-${today.getDate()}.json`;
-  const response = await fetch(url, {
-    cache: "no-cache",
-  });
-  const json = await response.json();
-  const meta = T.object({
-    id: T.number(),
-    solution: T.string(),
-    print_date: T.string(),
-    days_since_launch: T.number(),
-    editor: T.string(),
-  }).verify(json);
-  if (!meta) throw new Error("Invalid response from NYT.");
-  return {
-    solution: meta.solution.toUpperCase(),
-    day: meta.days_since_launch,
-    editor: meta.editor,
-  };
+  try {
+    const today = new Date();
+    const url = `https://www.nytimes.com/svc/wordle/v2/${today.getFullYear()}-${
+      today.getMonth() + 1
+    }-${today.getDate()}.json`;
+    const response = await fetch(url, {
+      cache: "no-cache",
+    });
+    const json = await response.json();
+    const meta = T.object({
+      id: T.number(),
+      solution: T.string(),
+      print_date: T.string(),
+      days_since_launch: T.number(),
+      editor: T.string(),
+    }).verify(json);
+    if (!meta) throw new Error("Invalid response from NYT.");
+    return {
+      solution: meta.solution.toUpperCase(),
+      day: meta.days_since_launch,
+      editor: meta.editor,
+    };
+  } catch (e) {
+    console.error(e);
+    throw new Error("Failed to get word of the day.");
+  }
 }
