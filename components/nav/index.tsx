c7a152bfdf6f94597afb4dc5e2fb089a0db7999c
Elijah Cobb <elijahjcobb@gmail.com>
2023-11-30 17:42:35 -0800

    title

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -4,8 +4,8 @@ import type { WordOfTheDay } from "@/lib/get-word-of-the-day";
 
 export function Nav(meta: WordOfTheDay) {
 	return <nav className={styles.container}>
-		<h1>Wordle {meta.day} by {meta.editor}</h1>
-		<a href="https://elijahcobb.dev" target={'_blank'} rel="noreferrer">
+		<h1>Wordle {meta.day}</h1>
+		<a href="https://elijahcobb.dev/ships" target={'_blank'} rel="noreferrer">
 			<FaInfoCircle />
 		</a>
 	</nav>
